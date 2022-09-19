4830dc4ed1e9d027e0162cca5c7603646e5bb83f
sohailtanveer01 <sohailtanveer769@gmail.com>
2022-09-19 19:40:57 +0530

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function(req,res){
+  let date = new Date();
+
+    // Check if the optional date parameter was provided
+    if (req.params.date) {
+        // Convert the date parameter to a string
+        let unixDate = +req.params.date;
+
+        // Check if the date passed is unix time. If it's not, use the date string provided
+        date = isNaN(unixDate) ? new Date(req.params.date) : new Date(unixDate);
+
+        // Check if the date created is valid. Throw an error if it's an invalid date
+        if (!(date instanceof Date) || isNaN(date.getTime())) return res.json({ error: "Invalid Date" });
+    }
+
+    // Return the unix and UTC time
+    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+})
+
+
+
+
+
+//empt date parameter
+app.get("/api/", function(req,res){
+  const date = new Date()
+  const unix = date.getTime()
+  const utc = date.toUTCString()
+  res.json({
+    "unix" : unix,
+    "utc" : utc
+  })
+})
 
 
 // listen for requests :)
